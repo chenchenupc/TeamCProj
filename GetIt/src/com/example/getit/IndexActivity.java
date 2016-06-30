b72ab9dcfcb8e01379f16f70a170a14d57e64f00
SylviaS11 <859700012@qq.com>
2016-06-30 18:50:10 +0800

    630

---
+++ GetIt/src/com/example/getit/IndexActivity.java
@@ -0,0 +1,13 @@
+package com.example.getit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IndexActivity extends Activity {
+	 @Override
+	 protected void onCreate(Bundle savedInstanceState) {
+	     super.onCreate(savedInstanceState);
+	     setContentView(R.layout.main_scr);
+
+}
+}
